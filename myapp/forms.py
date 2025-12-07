4fceea5341e2422ad1d49abbd312e72750cf4728
Leemart Macatangay <martmacatangay27@gmail.com>
2025-12-07 18:39:08 +0800

    Update project files

---
+++ myapp/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import CustomUser
+from .models import BirdPhoto
+from .models import Bird
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            self.add_error('confirm_password', "Passwords do not match.")
+
+class BirdPhotoForm(forms.ModelForm):
+    class Meta:
+        model = BirdPhoto
+        fields = ['photo']
+
+
